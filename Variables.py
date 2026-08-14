ddbe0804f74e83051d7d90642f9b289bf6af04ed
mandalavirup07-ux <mandalavirup07@gmail.com>
2026-08-15 00:07:02 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,17 @@
+# Variables
+# What is variables?
+# Ans:- It is like a containner where a piece of data is kept on that containner and the conatinner is known as Variables
+name = "Rahul"
+print(name) # here you will get output like this:- Rahul and while writing the data in the conatinner  you should give doubles quotes.
+
+# You can also do Calculation
+a = 20
+b = 30
+print(a+b) #  in calculation whatever you have given equation like adding, subtracting, multiply or division the output come according to the equation hile writing the data in the conatinner you dont give doubles quotes.
+
+# Python String length (How to find the characters in the string?)
+Python = "You are a nasty boy or girl"
+print(len(Python)) # here you will get output that how many characters are there in the world You are a nasty boy or girl and the len function will count the numbers of characters along withe the space  and while writing the data in the conatinner you should give doubles quotes.
+
+# How to calculate the characters using Input Function and len function?
+print(len(input("Name of the book that you are reading")))# here you will get output that how many characters are there in the world You are a nasty boy or girl and the len function will count the numbers of characters along withe the space.
