e2ee0cd69ba5b6fb2b95c099ab823f95e9bf599d
mandalavirup07-ux <mandalavirup07@gmail.com>
2026-08-15 23:19:01 +0530

    Delete tas55k.py

--- tas55k.py
+++
@@ -1,4 +0,0 @@
-(https://appbrewery.github.io/python-day1-demo/)print("Welcome to the Band Name Generator.")
-city = input("Which city did you grow up in?\n")
-pet = input("What is the name of a pet?\n")
-print("Your band name could be " + city + " " + pet)
